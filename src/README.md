186a744edbb8ef5ce7fd548608b600685c29b384
Rebecca Turner <637275@gmail.com>
2017-01-15 02:02:42 -0500

    tienreaotni

---
+++ src/README.md
@@ -0,0 +1,2 @@
+Simple SemVer is a simple utility for dealing with semantic versioning, notably
+incrementing build metadata.
--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,43 @@ struct SemVer<'p, 'm> {
     metadata:   Vec<&'m str>,
 }
 
+impl<'s> Vec<&'s str> {
+    fn is_blank(&self, arr: Vec<&str>) -> bool {
+        if arr.len() == 0 {
+            //empty
+            return true;
+        } else {
+            //iterate
+            for el in arr {
+                if el.len() != 0 {
+                    //got a string
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
+
 impl<'p, 'm> SemVer<'p, 'm> {
+
     fn into_string(&self) -> String {
-        let output: String = format!("{}.{}.{}",
+        let mut output: String = format!("{}.{}.{}",
             self.major, self.minor, self.patch);
-        if !self.prerelease.is_empty() {
-            output += "-".to_string() + self.prerelease.join(".");
+
+        if !self.prerelease {
+            output += &("-".to_owned() + &self.prerelease.join(".")[..]);
         }
+
         if !self.metadata.is_empty() {
-            output += "+" + self.metadata.join(".");
+            output += &("+".to_owned() + &self.metadata.join(".")[..]);
         }
+
+        output
     }
 
-    fn from_string(&self, input: str) -> SemVer {
-    }
+    //fn from_string(&self, input: str) -> SemVer<'p, 'm> {
+    //}
 
     fn new() -> SemVer<'p, 'm> {
         SemVer {
